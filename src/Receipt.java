3b0c478242cf463034a9b499df661005ccf16a33
RossIanFerguson <rossianferguson@gmail.com>
2023-09-06 16:11:40 +0100

    Projects

--- src/Receipt.java
+++ src/Receipt.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Reciept {
+public class Receipt {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println(" name of first item ");
